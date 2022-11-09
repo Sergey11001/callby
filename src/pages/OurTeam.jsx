94f5107a0b867fa6a6480893a5e08ca714413918
serge <nat.kononencko@yandex.ru>
2022-11-09 18:17:55 +0300

    refractor ourTeam

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -1,18 +1,8 @@
 import PersonPortret from "../components/PersonPortret";
 import React, {useState} from "react";
+import {listPersonOurTeam} from "../constants";
 
 
-const arr_person_full=[
-    {imgUrl:"/img/profile_photo/1.png",name:`Владимир`,surname:"Житов",job:"CЕO"},
-    {imgUrl:"/img/profile_photo/2.png",name:"Евгения",surname:"Житов",job:"Founder/CVO"},
-    {imgUrl:"/img/profile_photo/3.png",name:"Владимир",surname:"Отставная",job:"Founder/CMO"},
-    {imgUrl:"/img/profile_photo/4.png",name:"Владимир",surname:"Житов",job:"Founder/ Art-director"},
-    {imgUrl:"/img/profile_photo/5.png",name:"Мария",surname:"Отставная",job:"Graphic Designer"},
-    {imgUrl:"/img/profile_photo/6.png",name:"Мария",surname:"Житов",job:"HR архитектор"},
-    {imgUrl:"/img/profile_photo/10.png",name:"Мария",surname:"Отставная",job:"HR-менеджер"},
-    {imgUrl:"/img/profile_photo/8.png",name:"Мария",surname:"Захарова",job:"Middle Backend Rust Developer"},
-    {imgUrl:"/img/profile_photo/9.png",name:"Мария",surname:"Захарова",job:"Middle Frontend Rust Developer"},
-]
 const OurTeam=()=>{
     const [activePersonTeam,setActivePersonTeam]=useState(0)
     return(
@@ -27,7 +17,7 @@ const OurTeam=()=>{
                     </div>
                 </div>
                 <div className="team_list">
-                    {arr_person_full.map((item,i)=>
+                    {listPersonOurTeam.map((item,i)=>
                         <PersonPortret key={i} index={i}  {...item} activePerson={activePersonTeam} setActivePerson={setActivePersonTeam}/>
                     )}
                 </div>
